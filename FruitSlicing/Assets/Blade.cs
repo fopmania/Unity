dc8413da19d135c08f2c377038600ad85f313d41
Jun <jun.kim.dev@gmail.com>
2018-06-15 13:16:32 -0400

    no message

---
+++ FruitSlicing/Assets/Blade.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour {
+    public GameObject bladeTrailPrefab;
+
+
+    public bool isCutting = false;
+    private GameObject currentBladeTrail;
+    Rigidbody2D rb;
+    private CircleCollider2D circleCollieder;
+    Camera cam;
+
+    public float minCuttingVelocity = .001f;
+    private Vector2 prePos;
+
+    void Start()
+	{
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+        circleCollieder = GetComponent<CircleCollider2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0))
+        {
+            StartCutting();    
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            StopCutting();
+            
+        }
+        if(isCutting)
+        {
+            UpdateCut();
+        }
+	}
+
+    void UpdateCut()
+    {
+        Vector2 newPos = cam.ScreenToWorldPoint(Input.mousePosition);
+        rb.position = newPos;
+
+        float velocity = (newPos - prePos).magnitude * Time.deltaTime;
+        if(velocity > minCuttingVelocity)
+        {
+            circleCollieder.enabled = true;
+        }else{
+            circleCollieder.enabled = false;
+        }
+        prePos = newPos;
+    }
+
+    void StartCutting()
+    {
+        isCutting = true;
+        currentBladeTrail = Instantiate(bladeTrailPrefab, transform);
+        circleCollieder.enabled = false;
+    }
+
+    void StopCutting()
+    {
+        isCutting = false;
+        currentBladeTrail.transform.SetParent(null);
+        Destroy(currentBladeTrail, 2f);
+        circleCollieder.enabled = false;
+    }
+
+}
---
+++ FruitSlicing/Assets/Fruit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour {
+    public GameObject fruitSlicedPrefab;
+
+    public float startForce = 15f;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Blade")
+        {
+            
+            Vector3 dir = (collision.transform.position - transform.position).normalized;
+
+            Quaternion rot = Quaternion.LookRotation(dir);
+
+            GameObject go =  Instantiate(fruitSlicedPrefab, transform.position, rot );
+            Destroy(go, 3f);
+
+            Destroy(gameObject);
+
+        }
+    }
+}
